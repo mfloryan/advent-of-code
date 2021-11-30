583803f64d5542fab20e2e9c4cfb1eed149780d9
Marcin Floryan <mfloryan@users.noreply.github.com>
2021-11-30 13:49:31 +0100

    2020:01 - cleanup

--- 2020/01/day01.js
+++ 2020/01/day01.js
@@ -1,4 +1,7 @@
+const {loadNumbers} = require('../input')
 const fs = require('fs')
+const assert = require('assert/strict')
+
 let sampleInput = `1721
 979
 366
@@ -7,7 +10,6 @@ let sampleInput = `1721
 1456`
 
 let testInput = sampleInput.split('\n').map(x => Number.parseInt(x))
-
 
 function findPair(input) {
     for (let i = 0; i < input.length; i++) {
@@ -20,10 +22,9 @@ function findPair(input) {
 }
 
 let pair = findPair(testInput)
-console.log(pair[0] * pair[1])
+assert.equal(pair[0] * pair[1], 514579)
 
-let input = fs.readFileSync('01/input.txt');
-console.log(input)
-let pair2 = findPair(input.toString().split('\n').map(x => Number.parseInt(x)))
+let inputNumbers = loadNumbers('01/input.txt')
+let pair2 = findPair(inputNumbers)
 console.log(pair2)
 console.log(pair2[0]* pair2[1])
