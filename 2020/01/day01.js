8eb1866334ab17804bf0164b60330451fec1bf7b
Marcin Floryan <mfloryan@users.noreply.github.com>
2021-11-30 13:59:10 +0100

    2020:01 :white_check_mark::white_check_mark:

--- 2020/01/day01.js
+++ 2020/01/day01.js
@@ -11,12 +11,21 @@ let sampleInput = `1721
 
 let testInput = sampleInput.split('\n').map(x => Number.parseInt(x))
 
-function findPair(input) {
+function findPair(input, seed = 0) {
     for (let i = 0; i < input.length; i++) {
         for (let j = i; j < input.length; j++) {
-          if (input[i] + input[j] == 2020) {
+          if (input[i] + input[j] + seed == 2020) {
               return [ input[i], input[j] ]
           }
+        }
+    }
+}
+
+function findTriplet(input) {
+    for (let i = 0; i < input.length; i++) {
+        let pair = findPair(input, input[i])
+        if (pair) {
+            return [pair, input[i]];
         }
     }
 }
@@ -28,3 +37,7 @@ let inputNumbers = loadNumbers('01/input.txt')
 let pair2 = findPair(inputNumbers)
 console.log(pair2)
 console.log(pair2[0]* pair2[1])
+
+let triplet = findTriplet(inputNumbers)
+console.log(triplet)
+console.log(triplet[0][0] * triplet[0][1] * triplet[1])
