d256b15263b1adb913ce071ff0d69d10c1627f0a
Marcin Floryan <mfloryan@users.noreply.github.com>
2021-12-06 08:26:17 +0100

    2021:06 And now with far less code

--- 2021/06/day06.js
+++ 2021/06/day06.js
@@ -3,24 +3,13 @@ let lines = loadLines('06/input.txt')
 // lines = [`3,4,3,1,2`]
 
 let fish = lines[0].split(',').map(n => Number.parseInt(n))
-
-let fishStats = fish.reduce( (p,c) => { if (p[c]) p[c]++; else p[c]=1; return p }, {})
+let fishStats = fish.reduce( (p,c) => { p[c]++; return p }, new Array(9).fill(0))
 
 for (let i = 0; i < 256; i++) {
-    let newFishStats = {}
-    let bornFish = 0
-    for (const fishGroup in fishStats) {
-        if (Number.parseInt(fishGroup) === 0) {
-            if (newFishStats[6]) newFishStats[6] += fishStats[fishGroup]; else newFishStats[6] = fishStats[fishGroup];
-            bornFish = fishStats[fishGroup];
-        } else {
-            if (newFishStats[Number.parseInt(fishGroup - 1)]) newFishStats[Number.parseInt(fishGroup - 1)] += fishStats[fishGroup]; 
-            else newFishStats[Number.parseInt(fishGroup - 1)] = fishStats[fishGroup]; 
-        }
-    }
-    if (bornFish > 0) newFishStats[8] = bornFish
-    fishStats = newFishStats
-    if (i == 79) console.log("Day 01 - part 1:", Object.values(fishStats).reduce((p,c) => p+c, 0))
+    let zero = fishStats.shift()
+    fishStats[6] += zero
+    fishStats.push(zero)
+    if (i == 79) console.log("Day 06 - part 1:", fishStats.reduce((p,c) => p+c, 0))
 }
 
-console.log("Day 06 - part 2:", Object.values(fishStats).reduce((p,c) => p+c, 0))
+console.log("Day 06 - part 2:", fishStats.reduce((p,c) => p+c, 0))
