ec24724f3df0f65a016193edaa8db9e4eeb5e3a9
Marcin Floryan <mfloryan@users.noreply.github.com>
2021-12-08 22:02:38 +0100

    2015:06 :white_check_mark: :white_check_mark:

--- 2015/06/day06.js
+++ 2015/06/day06.js
@@ -4,7 +4,6 @@ let input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf8
 let lines = input.split('\n')
 
 function parseLine(line) {
-    console.log(line)
     let parts  = line.match(/([\w ]*) ([0-9]{1,3}),([0-9]{1,3}) through ([0-9]{1,3}),([0-9]{1,3})/)
     return {a: parts[1], 
         x1: Number.parseInt(parts[2]),
@@ -14,16 +13,26 @@ function parseLine(line) {
     }
 }
 
-function getLightsStatus(data) {
+let codePartOne = {
+    "turn on": x => 1,
+    "turn off": x => 0,
+    "toggle": x => x==1?0:1
+}
+
+let codePartTwo = {
+    "turn on": x => x+1,
+    "turn off": x => x > 0 ? x-1 :x,
+    "toggle": x => x+2,
+}
+
+function getLightsStatus(data, code) {
     let grid = new Array(1000)
     for (let i = 0; i < 1000; i++) grid[i] = new Array(1000).fill(0)
 
     data.forEach(d => {
         for (let y = d.y1; y <= d.y2; y++) {
             for (let x = d.x1; x <= d.x2; x++) {
-                if (d.a == "turn on") grid[y][x] = 1
-                if (d.a == "turn off") grid[y][x] = 0
-                if (d.a == "toggle") grid[y][x] = grid[y][x]==0?1:0
+                grid[y][x] = code[d.a](grid[y][x])
             }
         }
     })
@@ -31,4 +40,5 @@ function getLightsStatus(data) {
     return grid.map(r => r.reduce((p,c)=> p+c, 0)).reduce( (p,c)=> p+c, 0)
 }
 
-console.log(getLightsStatus(lines.map(parseLine)))
+console.log(getLightsStatus(lines.map(parseLine), codePartOne))
+console.log(getLightsStatus(lines.map(parseLine), codePartTwo))
