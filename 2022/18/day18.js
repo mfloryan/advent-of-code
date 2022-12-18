b0bf422e52d7e929d4953688b0bf5c8146a50ee6
Marcin Floryan <mfloryan@users.noreply.github.com>
2022-12-18 07:26:31 +0100

    2022-18: :white_check_mark: :white_check_mark:

--- 2022/18/day18.js
+++ 2022/18/day18.js
@@ -19,42 +19,74 @@ let lines = loadLines('18/input.txt')
 let data = lines.map(l => l.split(",").map(Number))
 
 const sides = [
-    [0,0,1],
-    [0,1,0],
-    [1,0,0],
-    [0,0,-1],
-    [0,-1,0],
-    [-1,0,0],
+    [0, 0, 1],
+    [0, 1, 0],
+    [1, 0, 0],
+    [0, 0, -1],
+    [0, -1, 0],
+    [-1, 0, 0],
 ]
 
-function cubeHere(c, side) {
-    return c => c[0] == side[0] && c[1] == side[1] && c[2] == side[2]
+function cubeHere(a, b) {
+    return a[0] == b[0] && a[1] == b[1] && a[2] == b[2]
+}
+
+function addPoints(a, b) {
+    return [a[0] + b[0], a[1] + b[1], a[2] + b[2]]
 }
 
 function countUncoveredSides(data) {
     let totalUncovered = 0
     for (const cube of data) {
-        let cubeSides = sides.map(s => [cube[0]+s[0], cube[1]+s[1], cube[2]+s[2]])
-        let covered = cubeSides.filter(side => data.some(cubeHere)).length
+        let cubeSides = sides.map(s => [cube[0] + s[0], cube[1] + s[1], cube[2] + s[2]])
+        let covered = cubeSides.filter(side => data.some(c => cubeHere(side, c))).length
         totalUncovered += (6 - covered)
     }
     return totalUncovered
 }
 
-function countExternalArea(droplets) {
+function solvePart2(droplets) {
     let bounds = droplets.reduce(
-    (p,c) => [[Math.min(p[0][0],c[0]), Math.max(p[0][1],c[0])],
-               [Math.min(p[1][0],c[1]), Math.max(p[1][1],c[1])],
-               [Math.min(p[2][0],c[2]), Math.max(p[2][1],c[2])]],
-    [[Infinity,-Infinity],[Infinity,-Infinity],[Infinity,-Infinity]])
+        (p, c) => [[Math.min(p[0][0], c[0]), Math.max(p[0][1], c[0])],
+        [Math.min(p[1][0], c[1]), Math.max(p[1][1], c[1])],
+        [Math.min(p[2][0], c[2]), Math.max(p[2][1], c[2])]],
+        [[Infinity, -Infinity], [Infinity, -Infinity], [Infinity, -Infinity]])
 
-    for (let x = bounds[0][0]; x <= bounds[0][1]; x++) {
-        for (let y = bounds[1][0]; y <= bounds[1][1]; y++) {
-            for (let z = bounds[2][0]; z <= bounds[2][1]; z++) {
+    let outside = []
+    let q = []
+    start = [0, 0, 0]
+    outside.push(start)
+    q.push(start)
+    while (q.length > 0) {
+        let cur = q.shift()
+        for (const dir of sides) {
+            let s = addPoints(cur, dir)
+            if (s[0] >= bounds[0][0] - 1 && s[1] >= bounds[1][0] - 1 && s[2] >= bounds[2][0] - 1 &&
+                s[0] <= bounds[0][1] + 1 && s[1] <= bounds[1][1] + 1 && s[2] <= bounds[2][1] + 1 &&
+                !droplets.some(d => cubeHere(d, s)) &&
+                !outside.some(d => cubeHere(d, s))) {
+                outside.push(s)
+                q.push(s)
             }
         }
     }
+
+    console.log(outside)
+
+    let area1 = 0
+    let area2 = 0
+
+    for (const cube of droplets) {
+        for (const dir of sides) {
+            area1 += !droplets.some(c => cubeHere(c, addPoints(cube, dir))) ? 1 : 0
+            area2 += (!droplets.some(c => cubeHere(c, addPoints(cube, dir))) && 
+                       outside.some(c => cubeHere(c, addPoints(cube, dir)))) ? 1 : 0
+        }
+    }
+
+    return [area1, area2]
 }
 
+
 console.log(countUncoveredSides(data))
-console.log(countExternalArea(data))
+console.log(solvePart2(data))
