0de95227a797d169268f02d9b6fed130b101a52d
Marcin Floryan <mfloryan@users.noreply.github.com>
2023-12-03 06:54:19 +0100

    2023-03: ⭐️⭐️

--- 2023/03/day03.js
+++ 2023/03/day03.js
@@ -1,3 +1,4 @@
+const { hasSubscribers } = require('diagnostics_channel')
 const fs = require('fs')
 const path = require('path')
 
@@ -24,21 +25,21 @@ function hasSymbol(map, max, number) {
     if (number.y1 > 0) {
         for (let x = Math.max(0, number.x1 - 1); x <= Math.min(max.x, number.x2 + 1); x++) {
             let point = map[number.y1 - 1][x].p;
-            if (point != '.') return point
+            if (point != '.') return {x, y:number.y1 - 1, symbol: point}
         }
     }
     if (number.x1 > 0) {
         let point = map[number.y1][number.x1 - 1].p
-        if (point != '.') return point
+        if (point != '.') return {x : number.x1 - 1, y:number.y1, symbol: point}
     }
     if (number.x2 < max.x) {
         let point = map[number.y1][number.x2 + 1].p
-        if (point != '.') return point
+        if (point != '.') return {x : number.x2 + 1, y:number.y2, symbol: point}
     }
     if (number.y2 < max.y) {
         for (let x = Math.max(0, number.x1 - 1); x <= Math.min(max.x, number.x2 + 1); x++) {
             let point = map[number.y2 + 1][x].p;
-            if (point != '.') return point
+            if (point != '.') return {x, y:number.y2 + 1, symbol: point}
         }
     }
     return false
@@ -84,6 +85,23 @@ for (let y = 0; y <= max.y; y++) {
     }
 }
 
-
 let part1 = numbers.filter(n => hasSymbol(map, max, n)).map(n => n.value).reduce((p,c) => p+c,0)
 console.log(part1)
+
+let symbols = []
+
+numbers.forEach(number => {
+    let symbol = hasSymbol(map, max, number)
+    if (symbol && symbol.symbol == '*') {
+        let existingSymbol = symbols.find(s => s.x == symbol.x && s.y == symbol.y)
+        if (existingSymbol) {
+            existingSymbol.n2 = number.value
+        } else {
+            symbols.push({x:symbol.x, y:symbol.y, n1: number.value})
+        }
+    }
+})
+
+let validGears = symbols.filter(s => s.n2)
+
+console.log(validGears.reduce((p,c) => p + (c.n1 * c.n2),0))
