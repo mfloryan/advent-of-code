ce16f778cf7652a777cda8f25c5fd3c5ec51275b
Marcin Floryan <mfloryan@users.noreply.github.com>
2023-12-06 06:27:53 +0100

    :art:

--- 2023/06/day06.js
+++ 2023/06/day06.js
@@ -7,31 +7,29 @@ function parse(input) {
     let [t, d] = input.split('\n')
     let time = t.split(' ')
     time.shift()
+    time = time.filter(_ => _ != '').map(_ => parseInt(_))
     let distance = d.split(' ')
     distance.shift()
+    distance = distance.filter(_ => _ != '').map(_ => parseInt(_))
 
-    return  {
-        time: time.filter(_ => _ != '').map(_ => parseInt(_)),
-        distance: distance.filter(_ => _ != '').map(_ => parseInt(_)),
-    }
+    return time.map((t, index) => { return { time: t, distance: distance[index] } })
 }
 
-let data = parse(input)
-let races = data.time.map((t,i) => [t, data.distance[i]])
+let races = parse(input)
 
 function waysToWin(time, distance) {
-    let count = 0;
+    let count = 0
 
-    for (let t = 0; t <= time; t++) {
-        let d = (t) * (time - t)
+    for (let t = 1; t < time; t++) {
+        let d = t * (time - t)
         if (d > distance) count++
     }
     return count
 }
 
-console.log(races.map(_ => waysToWin(_[0], _[1])).reduce((p,c) => p*c, 1))
+console.log(races.map(_ => waysToWin(_.time, _.distance)).reduce((p, c) => p * c, 1))
 
-let time = parseInt(races.map(r => r[0]).join(''))
-let distance = parseInt(races.map(r => r[1]).join(''))
+let time = parseInt(races.map(r => r.time).join(''))
+let distance = parseInt(races.map(r => r.distance).join(''))
 
 console.log(waysToWin(time, distance))
