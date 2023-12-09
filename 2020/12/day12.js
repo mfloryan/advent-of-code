50c1648b8a707acecbd049f78e75f104435d9933
Marcin Floryan <mfloryan@users.noreply.github.com>
2023-12-09 17:24:10 +0100

    2020-12: ⭐️ :art: objects not arrays

--- 2020/12/day12.js
+++ 2020/12/day12.js
@@ -6,18 +6,15 @@ let input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf8
 function parse(line) {
     let action = line[0]
     let value = parseInt(line.substring(1))
-    return [action, value]
+    return { action, value }
 }
 
 const moves = {
-    'N': (pos, len) => [pos[0], pos[1] - len],
-    'S': (pos, len) => [pos[0], pos[1] + len],
-    'E': (pos, len) => [pos[0] + len, pos[1]],
-    'W': (pos, len) => [pos[0] - len, pos[1]],
+    'N': (pos, len) => { return { x: pos.x, y: pos.y - len } },
+    'S': (pos, len) => { return { x: pos.x, y: pos.y + len } },
+    'E': (pos, len) => { return { x: pos.x + len, y: pos.y } },
+    'W': (pos, len) => { return { x: pos.x - len, y: pos.y } },
 }
-
-const leftTurns = ['N', 'W', 'S', 'E']
-const rightTurns = ['N', 'E', 'S', 'W']
 
 const turns = {
     'N': { 'L': 'W', 'R': 'E' },
@@ -32,17 +29,17 @@ function turn(facing, direction, angle) {
 }
 
 const howToAct = {
-    'N': (state, value) => [state[0], moves['N'](state[1], value)],
-    'S': (state, value) => [state[0], moves['S'](state[1], value)],
-    'E': (state, value) => [state[0], moves['E'](state[1], value)],
-    'W': (state, value) => [state[0], moves['W'](state[1], value)],
-    'L': (state, value) => [turn(state[0], 'L', value), state[1]],
-    'R': (state, value) => [turn(state[0], 'R', value), state[1]],
-    'F': (state, value) => [state[0], moves[state[0]](state[1], value)],
+    'N': (state, value) => { return { heading: state.heading, pos: moves['N'](state.pos, value) } },
+    'S': (state, value) => { return { heading: state.heading, pos: moves['S'](state.pos, value) } },
+    'E': (state, value) => { return { heading: state.heading, pos: moves['E'](state.pos, value) } },
+    'W': (state, value) => { return { heading: state.heading, pos: moves['W'](state.pos, value) } },
+    'L': (state, value) => { return { heading: turn(state.heading, 'L', value), pos: state.pos } },
+    'R': (state, value) => { return { heading: turn(state.heading, 'R', value), pos: state.pos } },
+    'F': (state, value) => { return { heading: state.heading, pos: moves[state.heading](state.pos, value) } },
 }
 
 function move(state, instruction) {
-    return howToAct[instruction[0]](state, instruction[1])
+    return howToAct[instruction.action](state, instruction.value)
 }
 
 // input = `F10
@@ -51,8 +48,9 @@ function move(state, instruction) {
 // R90
 // F11`
 
-const distance = (p1, p2) => Math.abs(p2[0] - p1[0]) + Math.abs(p2[1] - p1[1])
-
+const distance = (p1, p2) => Math.abs(p2.x - p1.x) + Math.abs(p2.y - p1.y)
+let start = { heading: 'E', pos: { x: 0, y: 0 } }
 let actions = input.split('\n').map(parse)
-let destination = actions.reduce((p, c) => move(p, c), ['E', [0, 0]])
-console.log(distance([0,0], destination[1]))
+
+let destination = actions.reduce((p, c) => move(p, c), start)
+console.log(distance(start.pos, destination.pos))
