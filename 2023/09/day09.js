1dc0391ec0ec302f3874824a3d1025a61ba2242f
Marcin Floryan <mfloryan@users.noreply.github.com>
2023-12-09 06:28:04 +0100

    2023-09: ⭐️⭐️

--- 2023/09/day09.js
+++ 2023/09/day09.js
@@ -6,18 +6,16 @@ let input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf8
 let lines = input.split('\n')
 let data = lines.map(l => l.split(' ').map(_ => parseInt(_)))
 
-// console.log(data)
-
 function extrapolateNextValue(history) {
     let rows = []
     rows[0] = history.slice()
     let rowIndex = 0
 
-    while (! rows[rowIndex].every(v => v == 0)) {
+    while (!rows[rowIndex].every(v => v == 0)) {
         let newRow = []
         let currentRow = rows[rowIndex]
-        for (let i =0; i <currentRow.length - 1; i++) {
-            newRow.push(currentRow[i+1] - currentRow[i])
+        for (let i = 0; i < currentRow.length - 1; i++) {
+            newRow.push(currentRow[i + 1] - currentRow[i])
         }
         rows.push(newRow)
         rowIndex++;
@@ -26,16 +24,40 @@ function extrapolateNextValue(history) {
     rows[rows.length - 1].push(0)
 
     for (let i = rows.length - 2; i >= 0; i--) {
-        rows[i].push(rows[i][rows[i].length - 1] + rows[i+1][rows[i+1].length - 1])
+        rows[i].push(rows[i][rows[i].length - 1] + rows[i + 1][rows[i + 1].length - 1])
     }
 
     return rows[0][rows[0].length - 1]
 }
 
+function extrapolatePreviousValue(history) {
+    let rows = []
+    rows[0] = history.slice()
+    let rowIndex = 0
 
-console.log(extrapolateNextValue([10,13,16,21,30,45]))
+    while (!rows[rowIndex].every(v => v == 0)) {
+        let newRow = []
+        let currentRow = rows[rowIndex]
+        for (let i = 0; i < currentRow.length - 1; i++) {
+            newRow.push(currentRow[i + 1] - currentRow[i])
+        }
+        rows.push(newRow)
+        rowIndex++;
+    }
 
-const sum = (p,c) => p+c
+    rows[rows.length - 1].unshift(0)
+
+    for (let i = rows.length - 2; i >= 0; i--) {
+        rows[i].unshift(rows[i][0] - rows[i + 1][0])
+    }
+
+    return rows[0][0]
+}
+
+const sum = (p, c) => p + c
 
 let part1 = data.map(row => extrapolateNextValue(row)).reduce(sum)
 console.log(part1)
+
+let part2 = data.map(row => extrapolatePreviousValue(row)).reduce(sum)
+console.log(part2)
