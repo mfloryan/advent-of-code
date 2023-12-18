0e48e086cd2d3389de1de59a4f2b89b451400f87
Marcin Floryan <marcin.floryan@gmail.com>
2023-12-18 08:25:35 +0100

    2023-18: ⭐⭐️

--- 2023/18/day18.js
+++ 2023/18/day18.js
@@ -3,31 +3,31 @@ const path = require('path')
 
 let input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf8' })
 
-const digging = {
-    'R': { x: 1, y: 0 },
-    'D': { x: 0, y: 1 },
-    'U': { x: 0, y: -1 },
-    'L': { x: -1, y: 0 },
-}
-
-const apply = (p1, p2) => { return { x: p1.x + p2.x, y: p1.y + p2.y } }
-
 function parse(line) {
     let [dir, steps, colour] = line.split(' ', 3)
-    return { dir, steps, colour: colour.substring(1, colour.length - 1) }
+    return { dir, distance: parseInt(steps), colour: colour.substring(1, colour.length - 1) }
 }
 
+const dirMap = { 0: 'R', 1: 'D', 2: 'L', 3: 'U' }
 
-function dig(plan) {
-    let map = []
-    let pos = { x: 0, y: 0 }
-    for (const p of plan) {
-        for (let i = 0; i < p.steps; i++) {
-            pos = apply(pos, digging[p.dir])
-            map.push({ x: pos.x, y: pos.y, c: p.colour })
-        }
-    }
-    return map
+function parse2(instructions) {
+    let distance = Number.parseInt(instructions.colour.substring(1, 6), 16)
+    let dir = dirMap[instructions.colour.substring(6)]
+    return { distance, dir }
+}
+const getEnd = {
+    'R': (s, v) => { return { x: s.x + v, y: s.y } },
+    'L': (s, v) => { return { x: s.x - v, y: s.y } },
+    'D': (s, v) => { return { x: s.x, y: s.y + v } },
+    'U': (s, v) => { return { x: s.x, y: s.y - v } },
+}
+
+function getCoordinates(plan) {
+    let coordinates = []
+    let start = { x: 0, y: 0 }
+    coordinates.push(start)
+    plan.reduce((p, c) => { let end = getEnd[c.dir](p, c.distance); coordinates.push(end); return end }, start)
+    return coordinates
 }
 
 // input = `R 6 (#70c710)
@@ -45,110 +45,22 @@ function dig(plan) {
 // L 2 (#015232)
 // U 2 (#7a21e3)`
 
+
+function shoelace(coords) {
+    let c = BigInt(0)
+    for (let i = 0; i < coords.length - 1; i++) {
+        c += (BigInt(coords[i].x) * BigInt(coords[i + 1].y) - BigInt(coords[i].y) * BigInt(coords[i + 1].x))
+    }
+    return c / BigInt(2)
+}
+
+function calculateArea(data) {
+    let coords = getCoordinates(data)
+    return shoelace(coords) + BigInt(data.reduce((p, c) => p + c.distance, 0) / 2) + BigInt(1)
+}
+
 let data = input.split('\n').map(parse)
-let map = dig(data)
-console.log(map.length)
 
-let dim = map.reduce(
-    (p, c) => { return { l: Math.min(c.x, p.l), t: Math.min(c.y, p.t), b: Math.max(c.y, p.b), r: Math.max(c.x, p.r) } },
-    { l: Infinity, t: Infinity, b: -Infinity, r: -Infinity })
-
-console.log(dim)
-
-function inside(map, point) {
-    let left = map.filter(_ => _.x < point.x && _.y == point.y).length
-    let top = map.filter(_ => _.x == point.x && _.y < point.y).length
-
-
-    // let countLeft = 0, countTop = 0
-    // if (left.length > 0) {
-    //     countLeft = 1
-    //     for (let i = 1; i < left.length; i++) {
-    //         if (Math.abs(left[i] - left[i - 1]) > 1) countLeft++;
-    //     }
-    // }
-
-    // if (top.length > 0) {
-    //     countTop = 1
-    //     for (let i = 1; i < top.length; i++) {
-    //         if (Math.abs(top[i] - top[i - 1]) > 1) countTop++;
-    //     }
-    // }
-
-
-    // for (const t of top) {
-
-    // }
-
-
-    return (left % 2 == 1 && top % 2 == 1)
-}
-
-// let insideCount = 0
-// for (let x = dim.l; x <= dim.r; x++) {
-//     for (let y = dim.t; y <= dim.b; y++) {
-//         let p = { x, y }
-//         // console.log(p, inside(map, p))
-//         if (inside(map, p)) insideCount++
-//     }
-// }
-
-// console.log(insideCount)
-
-// for (let y = dim.t; y <= dim.b; y++) {
-//     let row = []
-//     for (let x = dim.l; x <= dim.r; x++) {
-//         let p = { x, y }
-//         if (map.find(_ => p.x == _.x && p.y == _.y)) row.push('#')
-//         else {
-//             row.push('.')
-//         }
-//     }
-//     console.log(row.join(''))
-// }
-
-function findStart(map, dim) {
-    for (let y = dim.t; y <= dim.b; y++) {
-        for (let x = dim.l; x <= dim.r; x++) {
-            let left = map.filter(_ => _.x < x && _.y == y).length
-            let right = map.filter(_ => _.x > x && _.y == y).length
-            if (left == 1 && right == 1) return { x, y }
-        }
-    }
-}
-
-console.log()
-floodFill(map, findStart(map, dim))
-
-for (let y = dim.t; y <= dim.b; y++) {
-    let row = []
-    for (let x = dim.l; x <= dim.r; x++) {
-        let p = { x, y }
-        let inMap = map.find(_ => p.x == _.x && p.y == _.y)
-        if (inMap) {
-            if (inMap.c) row.push("#"); else row.push("*")
-        } else row.push('.')
-
-    }
-    console.log(row.join(''))
-}
-
-
-function floodFill(map, point) {
-    let queue = [point]
-
-    while (queue.length > 0) {
-        let point = queue.shift()
-
-        let mapPoint = map.find(_ => _.x == point.x && _.y == point.y)
-        if (!mapPoint) {
-            map.push(point)
-            queue.push({ x: point.x - 1, y: point.y })
-            queue.push({ x: point.x + 1, y: point.y })
-            queue.push({ x: point.x, y: point.y - 1 })
-            queue.push({ x: point.x, y: point.y + 1 })
-        }
-    }
-}
-
-console.log(map.length)
+console.log(calculateArea(data))
+console.log(calculateArea(data.map(parse2)))
+return
