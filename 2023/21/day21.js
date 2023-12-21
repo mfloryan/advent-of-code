0cf4b92a692334ff0b91ed5218fb29b1495311da
Marcin Floryan <marcin.floryan@gmail.com>
2023-12-21 21:44:29 +0100

    2023-21: ⭐⭐️

--- 2023/21/day21.js
+++ 2023/21/day21.js
@@ -20,21 +20,56 @@ function findNext(garden, start) {
         .map(_ => garden[_.y][_.x])
 }
 
+
 function walkGarden(garden, start, steps = 6) {
-    let gotTo = [start]
+    let gotTo = new Set([start])
 
     for (let i = 0; i < steps; i++) {
 
-        let optionsForNextStep = gotTo.flatMap(_ => findNext(garden, _))
+        let optionsForNextStep = new Set()
+        gotTo.forEach((v) => findNext(garden, v).forEach(_ => optionsForNextStep.add(_)))
 
-        let uniqueSteps = []
-        for (const o of optionsForNextStep) {
-            if (!uniqueSteps.includes(o)) uniqueSteps.push(o)
-        }
-        gotTo = uniqueSteps
+        gotTo = optionsForNextStep
 
     }
-    console.log(gotTo.length)
+    return gotTo.size
+}
+
+function findNext2(garden, start) {
+    return Object.values(directions)
+        .map(d => apply(d, start))
+        .filter(_ => {
+            let fixedX = _.x < 0 ? (garden[0].length - 1) + ((_.x + 1) % garden[0].length) : _.x % garden[0].length
+            let fixedY = _.y < 0 ? (garden.length - 1) + ((_.y + 1) % garden[0].length) : _.y % garden[0].length
+
+            return garden[fixedY][fixedX].c != '#'
+        })
+}
+
+const fromXY = (p) => (p.x + 10000000 + p.y + 10000000) * (p.x + 10000000 + p.y + 10000000 + 1) / 2 + p.y + 10000000
+const toXY = (v) => {
+    let w = Math.floor((Math.sqrt(8 * v + 1) - 1) / 2)
+    let y = v - w * (w + 1) / 2
+    let x = w - y
+    return { x: x - 10000000, y: y - 10000000 }
+}
+
+function walkGarden2(garden, start, steps = 6) {
+    let gotTo = [fromXY(start)]
+
+    for (let i = 0; i < steps; i++) {
+
+        let optionsForNextStep = []
+        for (const g of gotTo) {
+            for (const n of findNext2(garden, toXY(g))) {
+                if (!optionsForNextStep.includes(fromXY(n))) optionsForNextStep.push(fromXY(n))
+            }
+        }
+
+        gotTo = optionsForNextStep
+
+    }
+    return gotTo.length
 }
 
 // input = `...........
@@ -49,7 +84,22 @@ function walkGarden(garden, start, steps = 6) {
 // .##..##.##.
 // ...........`
 
-let garden = input.split('\n').map((r,y) => r.split('').map((_,x) => { return {x,y,c:_}}))
+let garden = input.split('\n').map((r, y) => r.split('').map((_, x) => { return { x, y, c: _ } }))
 let flatGarden = garden.flatMap(_ => _)
 
-console.log(walkGarden(garden, flatGarden.find(o => o.c =="S"), 64))
+console.log(walkGarden(garden, flatGarden.find(o => o.c == "S"), 64))
+
+let steps = 26501365;
+
+let x = [Math.floor(garden.length / 2), Math.floor(garden.length / 2) + garden.length, Math.floor(garden.length / 2) + 2 * garden.length]
+let y = [
+    walkGarden2(garden, flatGarden.find(o => o.c == "S"), x[0]),
+    walkGarden2(garden, flatGarden.find(o => o.c == "S"), x[1]),
+    walkGarden2(garden, flatGarden.find(o => o.c == "S"), x[2])
+]
+
+console.log(x.map((v, i) => `a * ${Math.pow(v, 2)} + b * ${v} + c = ${y[i]}`).join('\n'));
+
+console.log("Solve for a,b,c")
+
+console.log("Result:", 620348631910321)
