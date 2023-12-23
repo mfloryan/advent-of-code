e245ddd3eadcf85096d9b2708042963f9d7f2e6b
Marcin Floryan <marcin.floryan@gmail.com>
2023-12-23 10:45:06 +0100

    2023-23: ⭐⭐️

--- 2023/23/day23.js
+++ 2023/23/day23.js
@@ -33,12 +33,121 @@ function icyWalk(island, position, end, visited = [], paths = []) {
         return
     }
 
-    // visited.push(position)
     let next = findNext(island, position).filter(p => !visited.some(v => v.x == p.x && v.y == p.y))
     for (const n of next) {
         icyWalk(island, n, end, [...visited, position], paths)
     }
 }
+
+function findNext2(island, position) {
+    return directions
+        .map(d => apply(d, position))
+        .filter(_ => _.x >= 0 && _.y >= 0 && _.x < island[0].length && _.y < island.length)
+        .filter(_ => island[_.y][_.x].c != '#')
+        .map(_ => island[_.y][_.x])
+}
+
+
+function createGraph(island) {
+    let graph = new Map()
+
+    for (let y = 0; y < island.length; y++) {
+        for (let x = 0; x < island[0].length; x++) {
+            if (island[y][x].c == '#') continue;
+            let next = findNext2(island, { x, y }).map(_ => { return { p: _, d: 1 } })
+            graph.set(island[y][x], next)
+        }
+    }
+
+    return graph
+}
+
+function reduceGraph(g) {
+
+    let n
+    for (const key of g.keys()) {
+        let next = g.get(key)
+        if (next.length == 2) {
+            n = key
+            break
+        }
+    }
+
+    if (!n) return false
+
+    let childrenOfNodeToRemove = g.get(n)
+    if (childrenOfNodeToRemove) {
+        let [left, right] = childrenOfNodeToRemove;
+
+        let leftNode = g.get(left.p)
+        if (leftNode) {
+            let newLeftNodeChildren = leftNode.map(_ => {
+                if (_.p == n) return { p: right.p, d: _.d + right.d }
+                else return _
+            })
+            g.set(left.p, newLeftNodeChildren)
+        }
+
+        let rightNode = g.get(right.p)
+        if (rightNode) {
+            let newRightNodeChildren = rightNode.map(_ => {
+                if (_.p == n) return { p: left.p, d: _.d + left.d }
+                else return _
+            })
+            g.set(right.p, newRightNodeChildren)
+        }
+    }
+    g.delete(n)
+
+    return true
+}
+
+function icyWalkG(graph, start, end) {
+    let longest = 0
+    let stack = [[start, 0, new Set([start])]]
+
+    while (stack.length > 0) {
+        let [point, steps, visited] = stack.pop();
+
+        visited.add(point)
+
+        if (point.x == end.x && point.y == end.y) {
+            longest = Math.max(longest, steps);
+            continue
+        }
+
+        graph
+            .get(point)
+            .filter(p => !visited.has(p.p))
+            .forEach(p => stack.push([p.p, steps + p.d, new Set(visited)]))
+    }
+
+    return longest;
+}
+
+// input = `#.#####################
+// #.......#########...###
+// #######.#########.#.###
+// ###.....#.>.>.###.#.###
+// ###v#####.#v#.###.#.###
+// ###.>...#.#.#.....#...#
+// ###v###.#.#.#########.#
+// ###...#.#.#.......#...#
+// #####.#.#.#######.#.###
+// #.....#.#.#.......#...#
+// #.#####.#.#.#########v#
+// #.#...#...#...###...>.#
+// #.#.#v#######v###.###v#
+// #...#.>.#...>.>.#.###.#
+// #####v#.#.###v#.#.###.#
+// #.....#...#...#.#.#...#
+// #.#########.###.#.#.###
+// #...###...#...#...#.###
+// ###.###.#.###v#####v###
+// #...#...#.#.>.>.#.>.###
+// #.###.###.#.###.#.#v###
+// #.....###...###...#...#
+// #####################.#`
 
 let island = input.split('\n').map((r, y) => r.split('').map((_, x) => { return { x, y, c: _ } }))
 let flatIsland = island.flatMap(_ => _)
@@ -46,10 +155,11 @@ let flatIsland = island.flatMap(_ => _)
 let start = flatIsland.find(_ => _.y == 0 && _.c == '.')
 let end = flatIsland.find(_ => _.y == island.length - 1 && _.c == '.')
 
-console.log(start, end)
-
 let paths = []
 icyWalk(island, start, end, [], paths)
+console.log(paths.reduce((p, c) => Math.max(p, c), -Infinity))
 
-console.log(paths)
-console.log(paths.reduce((p,c) => Math.max(p,c), -Infinity))
+let g = createGraph(island)
+while (reduceGraph(g)) { }
+let r = icyWalkG(g, start, end)
+console.log(r)
