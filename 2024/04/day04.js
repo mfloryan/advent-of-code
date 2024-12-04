f1b04b3e4331c8e7fda81f4fb995c4ad4d16a7e4
Marcin Floryan <marcin.floryan@gmail.com>
2024-12-04 06:45:51 +0100

    2024:04 ⭐️⭐️

--- 2024/04/day04.js
+++ 2024/04/day04.js
@@ -41,7 +41,6 @@ function testXmas(map, position) {
 
 // console.log(testXmas(map, [9,9]))
 
-
 let count=0;
 for (let y=0; y < map.length; y++) {
     for (let x =0; x < map[0].length; x++) {
@@ -51,4 +50,38 @@ for (let y=0; y < map.length; y++) {
     }
 }
 
+console.log(count)
+
+
+let masOptions = [
+    [[0,0],[1,1],[2,2], [2,0],[1,1],[0,2]],
+    [[2,2],[1,1],[0,0], [2,0],[1,1],[0,2]],
+    [[0,0],[1,1],[2,2], [0,2],[1,1],[2,0]],
+    [[2,2],[1,1],[0,0], [2,0],[1,1],[0,2]],
+
+    [[0,0],[-1,-1],[-2,-2], [-2,0],[-1,-1],[0,-2]],
+    [[-2,-2],[-1,-1],[0,0], [-2,0],[-1,-1],[0,-2]],
+    [[0,0],[-1,-1],[-2,-2], [0,-2],[-1,-1],[-2,0]],
+    [[-2,-2],[-1,-1],[0,0], [-2,0],[-1,-1],[0,-2]],
+]
+
+function testMas(map, position) {
+    // console.log(position)
+    let pointOptions = masOptions.map(o => o.map(p => [p[0]+position[0], p[1]+position[1]]))
+    // console.log(pointOptions)
+    pointOptions = pointOptions.filter(o => o.every(p => (p[0] >= 0 && p[1] >= 0 && p[0] < map.length && p[1] < map[0].length)))
+    // console.log(pointOptions)
+    let l = pointOptions.map(o => o.map(p => map[p[1]][p[0]])).map(w => w.join('')).filter(w => w == 'MASMAS')
+    // console.log(position, l)
+    return l.length
+}
+
+count=0;
+for (let y=0; y < map.length; y++) {
+    for (let x =0; x < map[0].length; x++) {
+        if (map[y][x] =='M') {
+            count += testMas(map, [x,y])
+        }
+    }
+}
 console.log(count)
