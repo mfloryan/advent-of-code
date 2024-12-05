a881cf4660003bee3320d7d11c41301bc547f4bd
Marcin Floryan <marcin.floryan@gmail.com>
2024-12-05 06:27:07 +0100

    2024:05 ⭐️⭐️

--- 2024/05/day05.js
+++ 2024/05/day05.js
@@ -35,13 +35,13 @@ let input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf8
 let [p1, p2] = input.split('\n\n')
 
 let rules = p1.split('\n').map(l => l.split('|').map(v => parseInt(v)))
-let pages = p2.split('\n').map(l => l.split(',').map(v => parseInt(v)))
+let orders = p2.split('\n').map(l => l.split(',').map(v => parseInt(v)))
 
-function isCorrectOrder(page, rules) {
+function isCorrectOrder(order, rules) {
     for (const rule of rules) {
-        if (page.includes(rule[0]) && page.includes(rule[1])) {
-            let first = page.indexOf(rule[0])
-            let second = page.indexOf(rule[1])
+        if (order.includes(rule[0]) && order.includes(rule[1])) {
+            let first = order.indexOf(rule[0])
+            let second = order.indexOf(rule[1])
 
             if (second < first) return false
         }
@@ -49,5 +49,24 @@ function isCorrectOrder(page, rules) {
     return true
 }
 
-let correctOrders = pages.filter(p => isCorrectOrder(p, rules))
-console.log(correctOrders.map(a => a[Math.floor(a.length/2)]).reduce((p,c) => p+c))
+function fixOrder(order, rules) {
+    let relevantRules = rules.filter(r => order.includes(r[0]) && order.includes(r[1]))
+    return order.toSorted((a, b) => {
+        rule = relevantRules.find(r => r.includes(a) && r.includes(b))
+        return rule.indexOf(a) - rule.indexOf(b)
+    })
+}
+
+let part1 = orders
+    .filter(p => isCorrectOrder(p, rules))
+    .map(a => a[Math.floor(a.length/2)])
+    .reduce((p,c) => p+c)
+console.log(part1)
+
+let part2 = orders
+    .filter(p => !isCorrectOrder(p, rules))
+    .map(o => fixOrder(o, rules))
+    .map(a => a[Math.floor(a.length/2)])
+    .reduce((p,c) => p+c)
+
+console.log(part2)
