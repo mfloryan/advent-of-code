7444263f534df3e1ab63d8e3d1e89f37eb0a2ca3
Marcin Floryan <marcin.floryan@gmail.com>
2024-12-06 06:57:52 +0100

    2024:06: ⭐️⭐️

--- 2024/06/day06.js
+++ 2024/06/day06.js
@@ -15,11 +15,9 @@ let input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf8
 // ......#...`
 
 let map = input.split('\n').flatMap((l, row) => l.split('').flatMap((c, col) => { return { c: c, x: col, y: row } }))
+let map2 = input.split('\n').map(l => l.split(''))
 
 let guard = map.find(p => p.c == '^')
-
-let guardPos = { c: guard.c, x: guard.x, y: guard.y }
-
 
 const dir = {
     '^': { x: 0, y: -1 },
@@ -37,16 +35,31 @@ const turnRight = {
 
 function moveOnMap(map, start, heading) {
     let distinctPositions = []
+    let walkingMap = map.map(l => new Array(l.length))
+    walkingMap[start.y][start.x] = true
     let end = false
     let currentHeading = heading
-    let currentPos = {x: start.x, y: start.y}
-    let bounds = map.reduce((p,c) => { return {x: Math.max(c.x, p.x), y: Math.max(c.y, p.y)}}, {x:0, y:0})
-    console.log(bounds)
+    let currentPos = { x: start.x, y: start.y }
+    let bounds = { x: map[0].length - 1, y: map.length - 1 }
+
+    let distinctPosLen = distinctPositions.length
+    let i = 0
 
     do {
+        i++
+        i = i % (map.length * 4)
 
-        if (!distinctPositions.find(p=> p.x == currentPos.x && p.y == currentPos.y)) {
-            distinctPositions.push({x:currentPos.x, y:currentPos.y})
+        if (!walkingMap[currentPos.y][currentPos.x]) {
+            walkingMap[currentPos.y][currentPos.x] = true
+            distinctPositions.push({ x: currentPos.x, y: currentPos.y })
+        }
+
+        if (i == 0) {
+            if (distinctPosLen == distinctPositions.length) {
+                return -1
+            } else {
+                distinctPosLen = distinctPositions.length
+            }
         }
 
         let offset = dir[currentHeading]
@@ -54,8 +67,8 @@ function moveOnMap(map, start, heading) {
         if (next.x < 0 || next.y < 0 || next.x > bounds.x || next.y > bounds.y) {
             end = true
         } else {
-            let nextOnMap = map.find(p => p.x == next.x && p.y == next.y)
-            if (nextOnMap.c == '#') {
+            let nextOnMap = map[next.y][next.x]
+            if (nextOnMap == '#') {
                 currentHeading = turnRight[currentHeading]
             } else {
                 currentPos.x = next.x
@@ -67,5 +80,23 @@ function moveOnMap(map, start, heading) {
     return distinctPositions.length
 }
 
+function testObstacles(map, start, heading) {
+    let possibleObstacles = 0
+    for (let x = 0; x < map[0].length; x++) {
+        for (let y = 0; y < map.length; y++) {
+            if (map[y][x] != '#' && map[y][x] != '^') {
 
-console.log(moveOnMap(map, { x: guard.x, y: guard.y }, guard.c))
+                let newMap = []
+                newMap = map.map(l => l.slice())
+
+                newMap[y][x] = '#'
+                let result = moveOnMap(newMap, start, heading)
+                if (result == -1) possibleObstacles++
+            }
+        }
+    }
+    return possibleObstacles
+}
+
+console.log(moveOnMap(map2, { x: guard.x, y: guard.y }, guard.c))
+console.log(testObstacles(map2, { x: guard.x, y: guard.y }, guard.c))
