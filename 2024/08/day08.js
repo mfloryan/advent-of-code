393af733268af498d9facf1bfe2c9eccc86c12c5
Marcin Floryan <marcin.floryan@gmail.com>
2024-12-08 06:49:33 +0100

    2024:08: ⭐️⭐

--- 2024/08/day08.js
+++ 2024/08/day08.js
@@ -3,40 +3,50 @@ const path = require('path')
 
 let input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf8' })
 
-let map = input.split('\n').flatMap((l, row) => l.split('').flatMap((c, col) => { return { c: c, x: col, y: row } }))
-let map2 = input.split('\n').map(l => l.split(''))
+function parseMapIntoList(input) {
+    return input
+        .split('\n')
+        .flatMap(
+            (l, row) => l.split('')
+            .map((c, col) => { return { c: c, x: col, y: row } }))
+}
 
-let freq = map.filter(m => m.c != '.')
-let freqGroups = Object.groupBy(freq, v => v.c)
-
-function distance(a,b) {
-    return Math.abs(a.x - b.x) + Math.abs(a.y - b.y)
+function getBounds(map) {
+    return map.reduce((p, c) => { 
+        return { x: Math.max(p.x, c.x), y: Math.max(p.y, c.y) } },
+        { x: 0, y: 0 })
 }
 
 function checkBounds(p, bounds) {
     return p.x >= 0 && p.y >= 0 && p.x <= bounds.x && p.y <= bounds.y
 }
 
-function calculatePairAntinodes(a,b, bounds) {
+function sameNode(a, b) {
+    return a.x == b.x && a.y == b.y
+}
+
+function calculatePairAntinodes(a, b, bounds, minDist = 2, maxDist = 2) {
     let dx = a.x - b.x
     let dy = a.y - b.y
-    let a1 = {x: a.x - 2*dx, y: a.y - 2*dy}
-    let a2 = {x: b.x + 2* dx, y: b.y + 2*dy}
-    let ret = []
-    if (checkBounds(a1, bounds)) ret.push(a1)
-    if (checkBounds(a2, bounds)) ret.push(a2)
+
+    let as = []
+    for (let m = minDist; m < maxDist + 1; m++) {
+        as.push({ x: a.x - m * dx, y: a.y - m * dy })
+        as.push({ x: b.x + m * dx, y: b.y + m * dy })
+    }
+    let ret = as.filter(a => checkBounds(a, bounds))
     return ret
 }
 
-function getAntiNodes(groups, bounds) {
+function getAntiNodes(groups, bounds, minDist = 2, maxDist = 2) {
     let antinodes = []
     for (const freq of Object.keys(groups)) {
         let group = groups[freq]
         for (let i = 0; i < group.length; i++) {
-            for (let j = i+1; j < group.length; j++) {
-                let a = calculatePairAntinodes(group[i], group[j], bounds)
+            for (let j = i + 1; j < group.length; j++) {
+                let a = calculatePairAntinodes(group[i], group[j], bounds, minDist, maxDist)
                 for (const node of a) {
-                    if (!antinodes.some(n => n.x == node.x && n.y == node.y)) {
+                    if (!antinodes.some(n => sameNode(node, n))) {
                         antinodes.push(node)
                     }
                 }
@@ -46,9 +56,10 @@ function getAntiNodes(groups, bounds) {
     return antinodes
 }
 
-function parseGroups(groups, bounds) {
-    return getAntiNodes(groups, bounds)
-}
+let map = parseMapIntoList(input)
+let freq = map.filter(m => m.c != '.')
+let freqGroups = Object.groupBy(freq, v => v.c)
 
-let bounds = map.reduce((p,c) => {return {x:Math.max(p.x, c.x), y: Math.max(p.y, c.y)}}, {x:0, y:0})
-console.log(parseGroups(freqGroups, bounds).length)
+let bounds = getBounds(map)
+console.log(getAntiNodes(freqGroups, bounds).length)
+console.log(getAntiNodes(freqGroups, bounds, 1, bounds.x).length)
