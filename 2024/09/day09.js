52ba2b810ae153307acedb0aa1a5b7fd342e1c02
Marcin Floryan <marcin.floryan@gmail.com>
2024-12-09 07:01:45 +0100

    2024:09: ⭐️⭐️- reformatted

--- 2024/09/day09.js
+++ 2024/09/day09.js
@@ -20,10 +20,27 @@ function parseDisk(disk) {
         }
         file = !file
     }
-    return layout   
+    return layout
 }
 
-function compact(disk) {
+function parseDisk2(disk) {
+    let layout = []
+
+    let file = true
+    let blockId = 0
+    for (const block of disk) {
+        if (file) {
+            layout.push({ size: block, id: blockId })
+            blockId++
+        } else {
+            layout.push({ size: block, id: -1 })
+        }
+        file = !file
+    }
+    return layout
+}
+
+function defrag(disk) {
     let done = false
     do {
         let firstFreeSpace = disk.indexOf(-1)
@@ -37,58 +54,32 @@ function compact(disk) {
     return disk
 }
 
-layout = parseDisk(disk)
-newLayout = compact(layout)
-console.log(newLayout.reduce((p,c,i) => p + c * i,0))
-
-function parseDisk2(disk) {
-    let layout = []
-
-    let file = true
-    let blockId = 0
-    for (const block of disk) {
-        if (file) {
-            layout.push({size: block, id: blockId})
-            blockId++
-        } else {
-            layout.push({size: block, id: -1})
-        }
-        file = !file
-    }
-    return layout   
-}
-
 function defrag2(disk) {
-    let maxFileId = disk.reduce((p,c) => Math.max(p, c.id),0)
+    let maxFileId = disk.reduce((p, c) => Math.max(p, c.id), 0)
     for (let id = maxFileId; id > 0; id--) {
         fileIndex = disk.findIndex(s => s.id == id)
-        if (fileIndex == -1) {
-            throw "File not found expection"
-        }
         let file = disk[fileIndex]
+        
         let newFreeSpaceIndex = disk.findIndex(v => v.id == -1 && v.size >= file.size)
         if (newFreeSpaceIndex != -1 && newFreeSpaceIndex < fileIndex) {
-            disk[fileIndex] = {id:-1, size: file.size}
+            disk[fileIndex] = { id: -1, size: file.size }
             let freeSpace = disk[newFreeSpaceIndex]
             if (file.size == freeSpace.size) {
                 disk.splice(newFreeSpaceIndex, 1, file)
             } else {
-                disk.splice(newFreeSpaceIndex, 1, file, {id: -1, size: freeSpace.size - file.size} )
+                disk.splice(newFreeSpaceIndex, 1, file, { id: -1, size: freeSpace.size - file.size })
             }
         }
     }
 }
-
-l2 = parseDisk2(disk)
-nl2 = defrag2(l2)
 
 function countChecksum(disk) {
     let checksum = 0
     let blockId = 0
     for (const space of disk) {
         if (space.id > 0) {
-            for (let i=0; i < space.size; i++){
-                checksum += (i+blockId) * space.id
+            for (let i = 0; i < space.size; i++) {
+                checksum += (i + blockId) * space.id
             }
             blockId += space.size
         } else {
@@ -98,4 +89,10 @@ function countChecksum(disk) {
     return checksum
 }
 
+layout = parseDisk(disk)
+newLayout = defrag(layout)
+console.log(newLayout.reduce((p, c, i) => p + c * i, 0))
+
+l2 = parseDisk2(disk)
+nl2 = defrag2(l2)
 console.log(countChecksum(l2))
