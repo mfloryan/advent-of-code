268f9bc08ac719eb30d034b78ab93ae6212b946f
Marcin Floryan <marcin.floryan@gmail.com>
2024-12-16 21:59:21 +0100

    2017:17 ⭐️⭐️

--- 2017/17/day17.js
+++ 2017/17/day17.js
@@ -1,14 +1,30 @@
-let input = 366
 
-let buffer = [0]
+function evaluateBuffer(spins) {
+    let buffer = [0]
 
-let pos = 0
-for (let i=1; i <= 2017; i++) {
-    pos += input
-    pos %= buffer.length
-    buffer.splice(pos+1,0,i)
-    pos++
+    let pos = 0
+    for (let i = 1; i <= 2017; i++) {
+        pos += spins
+        pos %= buffer.length
+        buffer.splice(pos + 1, 0, i)
+        pos++
+    }
+    return buffer
 }
 
-console.log(buffer[buffer.indexOf(2017)+1])
+function evaluateBuffer2(spins) {
+    let position = 0
+    let lastValue = 0
+    for (let i = 1; i <= 50000000; i++) {
+        position = ((position + spins) % i) + 1;
+        if (position == 1)
+            lastValue = i
+    }
+    return lastValue
+}
 
+let input = 366
+
+let buffer = evaluateBuffer(input)
+console.log(buffer[buffer.indexOf(2017) + 1])
+console.log(evaluateBuffer2(input))
