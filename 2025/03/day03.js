52ea223e91085841bb1844ae05acfe6acd5c24c5
Marcin Floryan <marcin.floryan@gmail.com>
2025-12-03 06:41:10 +0100

    2025 day 03: ⭐️⭐️ - optimised

--- 2025/03/day03.js
+++ 2025/03/day03.js
@@ -4,33 +4,13 @@ const path = require('path')
 let input = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf8' })
 
 
-function findMaxVoltage(bank) {
-    let firstMax = 0
-    let firstPos = 0
-    for (let i = 0; i < bank.length - 1; i++) {
-        if (bank[i] > firstMax) {
-            firstMax = bank[i]
-            firstPos = i
-        }
-    }
-
-    let second = 0
-    for (let i = firstPos + 1; i < bank.length; i++) {
-        if (bank[i] > second) second = bank[i]
-    }
-
-    return firstMax * 10 + second
-}
-
-function findMaxJoltedVoltage(bank) {
+function findMaxJoltedVoltage(bank, len) {
     let selected = []
     let pos = -1
-    for (let j = 11; j >= 0; j--) {
-        // console.log(j, pos)
+    for (let j = len; j > 0; j--) {
         let max = 0
 
-        for (let i = pos + 1; i < bank.length - j; i++) {
-            // console.log("  ",i, bank[i])
+        for (let i = pos + 1; i < bank.length - (j - 1); i++) {
             if (bank[i] > max) {
                 max = bank[i]
                 pos = i
@@ -41,6 +21,6 @@ function findMaxJoltedVoltage(bank) {
     return parseInt(selected.join(''))
 }
 
-let banks = input.split('\n').map(l => l.split('').map(v => parseInt(v)))
-console.log(banks.map(b => findMaxVoltage(b)).reduce((p, c) => p + c, 0))
-console.log(banks.map(b => findMaxJoltedVoltage(b)).reduce((p, c) => p + c, 0))
+let banks = input.split('\n').map(l => l.split('').map(v => Number.parseInt(v)))
+console.log(banks.map(b => findMaxJoltedVoltage(b, 2)).reduce((p, c) => p + c, 0))
+console.log(banks.map(b => findMaxJoltedVoltage(b, 12)).reduce((p, c) => p + c, 0))
